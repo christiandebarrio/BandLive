e026a980037c66bcb2f3475566bf8d6c48bc0822
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-15 00:23:52 +0100

    next concerts order ASC

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
 
   def index
-    @concerts = Concert.all
+    @concerts = Concert.next_concerts.order_by_date
   end
 end
--- app/models/concert.rb
+++ app/models/concert.rb
@@ -11,4 +11,8 @@ class Concert < ActiveRecord::Base
   def self.order_by_date
     order('date ASC')
   end
+
+  def self.next_concerts
+    where('date >= ?', Date.today)
+  end
 end
