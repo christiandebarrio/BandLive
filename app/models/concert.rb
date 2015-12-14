cc3ac55208e93aff67c33b81040e53f4f7569c1d
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-14 14:16:35 +0100

    validates presence

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -2,6 +2,8 @@ class Concert < ActiveRecord::Base
   belongs_to :band
   belongs_to :venue
 
+  validates :band_id, :venue_id, :date, :time, presence: true
+
   def self.order_by_date
     order('date ASC')
   end
