6e89eec29c034ff7edb26f46087c8adc57698c49
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-14 14:17:46 +0100

    validates presence

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,8 @@
 class Participant < ActiveRecord::Base
   belongs_to :band
 
+  validates :name, :email, :instrument, presence: true
+
   def generate_photo_url name
     photo_file = name.downcase.strip.gsub(" ","-")
     photo_url = "participants/" + photo_file + ".jpg"
