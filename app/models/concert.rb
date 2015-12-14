0357c32c756588776c78056eb46bdfbe91f7757b
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-14 15:33:19 +0100

    validate 1 concert per band or venue

--- app/models/concert.rb
+++ app/models/concert.rb
@@ -3,6 +3,10 @@ class Concert < ActiveRecord::Base
   belongs_to :venue
 
   validates :band_id, :venue_id, :date, :time, presence: true
+  validates :date, uniqueness: { scope: :band_id,
+    message: "This band have a concert in this day yet" }
+  validates :date, uniqueness: { scope: :venue_id,
+    message: "This venue have a concert in this day yet" }
 
   def self.order_by_date
     order('date ASC')
