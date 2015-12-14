13eada4ecfae5c605fcf4e99cf9b3a4e067ed156
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-14 14:15:10 +0100

    validates

--- app/models/band.rb
+++ app/models/band.rb
@@ -2,6 +2,8 @@ class Band < ActiveRecord::Base
   has_many :participants
   has_many :concerts
 
+  validates :name, :gender, :bandtype, :city, :language, presence: true
+
   def generate_photo_url name
     photo_file = name.downcase.strip.gsub(" ","-")
     photo_url = "bands/" + photo_file + ".jpg"
