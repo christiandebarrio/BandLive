cd82a017102b31bdf92b35e5d9f9f62135937986
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2015-12-16 13:05:13 +0100

    refactor

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,12 +2,6 @@ class VenuesController < ApplicationController
 
   def show
     @venue = Venue.find_by_id(params[:id])
-
-    # if params[:search].present?
-    #   @locations = Location.near(params[:search], 50, :order => :distance)
-    # else
-    #   @locations = Location.all
-    # end
   end
 
   def index
