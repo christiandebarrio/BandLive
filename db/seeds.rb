577f614b7d1184f37711b207d4ffbda23c95897c
Christian de Barrio Arribas <christiandebarrio@gmail.com>
2017-03-30 19:41:14 +0200

    config

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,34 +8,34 @@
 
 description_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tincidunt dolor in velit molestie, at finibus risus fermentum. Quisque ultrices semper purus imperdiet viverra. In at est libero. Maecenas nisi justo, iaculis a convallis non, varius vitae enim. Vestibulum vitae leo sit amet metus sagittis gravida. Morbi nunc lacus, ornare vel metus in, lacinia mattis turpis. Praesent non tempor nisl, sed dignissim nunc."
 
-Band.create(name: "Topper Chopper", 
-            gender: "rock", 
-            bandtype: "covers", 
-            city: "Madrid", 
+Band.create(name: "Topper Chopper",
+            gender: "rock",
+            bandtype: "covers",
+            city: "Madrid",
             language: "english & spanish",
             photo: "bands/topper-chopper.jpg",
             description: description_text)
 
-Band.create(name: "Leroy", 
-            gender: "pop", 
-            bandtype: "original", 
-            city: "Cuenca", 
+Band.create(name: "Leroy",
+            gender: "pop",
+            bandtype: "original",
+            city: "Cuenca",
             language: "english",
             photo: "bands/leroy.jpg",
             description: description_text)
 
-Band.create(name: "Colors", 
-            gender: "jazz", 
-            bandtype: "covers", 
-            city: "Madrid", 
+Band.create(name: "Colors",
+            gender: "jazz",
+            bandtype: "covers",
+            city: "Madrid",
             language: "english",
             photo: "bands/colors.jpg",
             description: description_text)
 
-Band.create(name: "Divinities", 
-            gender: "pop-rock", 
-            bandtype: "original", 
-            city: "Madrid", 
+Band.create(name: "Divinities",
+            gender: "pop-rock",
+            bandtype: "original",
+            city: "Madrid",
             language: "english",
             photo: "bands/divinities.jpg",
             description: description_text)
@@ -45,53 +45,53 @@ leroy = Band.find_by_name("Leroy")
 colors = Band.find_by_name("Colors")
 divinities = Band.find_by_name("Divinities")
 
-Participant.create(name: "Francis", 
-                   email: "francisvitro@gmail.com", 
+Participant.create(name: "Francis",
+                   email: "francisvitro@gmail.com",
                    instrument: "drums",
                    photo: "participants/francis.jpg")
 
-Participant.create(name: "Eric", 
-                   email: "eric@email.com", 
+Participant.create(name: "Eric",
+                   email: "eric@email.com",
                    instrument: "guitar & vocals",
                    photo: "participants/eric.jpg")
 
-Participant.create(name: "Fernando", 
-                   email: "fernando@email.com", 
+Participant.create(name: "Fernando",
+                   email: "fernando@email.com",
                    instrument: "keyboards",
                    photo: "participants/fernando.jpg")
 
-Participant.create(name: "Julia", 
-                   email: "julia@email.com", 
+Participant.create(name: "Julia",
+                   email: "julia@email.com",
                    instrument: "guitar & vocals",
                    photo: "participants/julia.jpg")
 
-Participant.create(name: "Angy", 
-                   email: "angy@email.com", 
+Participant.create(name: "Angy",
+                   email: "angy@email.com",
                    instrument: "bass",
                    photo: "participants/angy.jpg")
 
-Participant.create(name: "Fred", 
-                   email: "fred@email.com", 
+Participant.create(name: "Fred",
+                   email: "fred@email.com",
                    instrument: "bass",
                    photo: "participants/fred.jpg")
 
-Participant.create(name: "Pablo", 
-                   email: "pablo@email.com", 
+Participant.create(name: "Pablo",
+                   email: "pablo@email.com",
                    instrument: "violin",
                    photo: "participants/pablo.jpg")
 
-Participant.create(name: "Ana", 
-                   email: "ana@email.com", 
+Participant.create(name: "Ana",
+                   email: "ana@email.com",
                    instrument: "drums",
                    photo: "participants/ana.jpg")
 
-Participant.create(name: "Marta", 
-                   email: "marta@email.com", 
+Participant.create(name: "Marta",
+                   email: "marta@email.com",
                    instrument: "bass",
                    photo: "participants/marta.jpg")
 
-Participant.create(name: "Irma", 
-                   email: "irma@email.com", 
+Participant.create(name: "Irma",
+                   email: "irma@email.com",
                    instrument: "guitar & vocals",
                    photo: "participants/irma.jpg")
 
@@ -130,71 +130,71 @@ Venue.create(name: "Cafe Populart",
              photo: "venues/cafe-populart.jpg",
              description: description_text)
 
-# Venue.create(name: "Soul Station",
-#              email: "soulstation@email.com",
-#              address: "Cuesta Santo Domingo, 22, 28013 Madrid",
-#              photo: "venues/soul-station.jpg",
-#              description: description_text)
+Venue.create(name: "Soul Station",
+             email: "soulstation@email.com",
+             address: "Cuesta Santo Domingo, 22, 28013 Madrid",
+             photo: "venues/soul-station.jpg",
+             description: description_text)
 
-# Venue.create(name: "La Boca del Lobo",
-#              email: "labocalobo@email.com",
-#              address: "Calle de Argumosa, 11, 28012 Madrid",
-#              photo: "venues/la-boca-del-lobo.jpg",
-#              description: description_text)
+Venue.create(name: "La Boca del Lobo",
+             email: "labocalobo@email.com",
+             address: "Calle de Argumosa, 11, 28012 Madrid",
+             photo: "venues/la-boca-del-lobo.jpg",
+             description: description_text)
 
-# Venue.create(name: "La Coquette",
-#              email: "lacoquette@email.com",
-#              address: "Calle de las Hileras, 14, 28013 Madrid",
-#              photo: "venues/la-coquette.jpg",
-#              description: description_text)
+Venue.create(name: "La Coquette",
+             email: "lacoquette@email.com",
+             address: "Calle de las Hileras, 14, 28013 Madrid",
+             photo: "venues/la-coquette.jpg",
+             description: description_text)
 
-# Venue.create(name: "El Chico Feo",
-#              email: "elchicofeo@email.com",
-#              address: "Calle Covarrubias, 21, 28010 Madrid",
-#              photo: "venues/el-chico-feo.jpg",
-#              description: description_text)
+Venue.create(name: "El Chico Feo",
+             email: "elchicofeo@email.com",
+             address: "Calle Covarrubias, 21, 28010 Madrid",
+             photo: "venues/el-chico-feo.jpg",
+             description: description_text)
 
 moby_dick = Venue.find_by_name("Moby Dick")
 honky_tonk = Venue.find_by_name("Honky Tonk")
 cafe_leka_leka = Venue.find_by_name("Cafe Leka Leka")
 cafe_populart = Venue.find_by_name("Cafe Populart")
-# soul_station = Venue.find_by_name("Soul Station")
-# la_boca_del_lobo = Venue.find_by_name("La Boca del Lobo")
-# la_coquette = Venue.find_by_name("La Coquette")
-# el_chico_feo = Venue.find_by_name("El Chico Feo")
+soul_station = Venue.find_by_name("Soul Station")
+la_boca_del_lobo = Venue.find_by_name("La Boca del Lobo")
+la_coquette = Venue.find_by_name("La Coquette")
+el_chico_feo = Venue.find_by_name("El Chico Feo")
 
-# Concert.create(band_id: topper_chopper.id,
-#                venue_id: honky_tonk.id,
-#                date: "6/12/2015",
-#                time: "20:00")
+Concert.create(band_id: leroy.id,
+               venue_id: honky_tonk.id,
+               date: "21/03/2017",
+               time: "23:00")
 
-# Concert.create(band_id: leroy.id,
-#                venue_id: moby_dick.id,
-#                date: "15/12/2015",
-#                time: "21:00")
+Concert.create(band_id: topper_chopper.id,
+               venue_id: honky_tonk.id,
+               date: "6/04/2017",
+               time: "20:00")
 
-# Concert.create(band_id: colors.id,
-#                venue_id: honky_tonk.id,
-#                date: "16/12/2015",
-#                time: "20:00")
+Concert.create(band_id: leroy.id,
+               venue_id: moby_dick.id,
+               date: "15/04/2017",
+               time: "21:00")
 
-# Concert.create(band_id: leroy.id,
-#                venue_id: moby_dick.id,
-#                date: "16/12/2015",
-#                time: "22:00")
+Concert.create(band_id: colors.id,
+               venue_id: honky_tonk.id,
+               date: "16/04/2017",
+               time: "20:00")
 
-# Concert.create(band_id: colors.id,
-#                venue_id: el_chico_feo.id,
-#                date: "16/12/2015",
-#                time: "22:00")
+Concert.create(band_id: leroy.id,
+               venue_id: moby_dick.id,
+               date: "16/04/2017",
+               time: "22:00")
 
-# Concert.create(band_id: topper_chopper.id,
-#                venue_id: honky_tonk.id,
-#                date: "20/12/2015",
-#                time: "20:00")
+Concert.create(band_id: colors.id,
+               venue_id: el_chico_feo.id,
+               date: "16/04/2017",
+               time: "22:00")
 
-# Concert.create(band_id: leroy.id,
-#                venue_id: honky_tonk.id,
-#                date: "21/12/2015",
-#                time: "23:00")
+Concert.create(band_id: topper_chopper.id,
+               venue_id: honky_tonk.id,
+               date: "20/04/2017",
+               time: "20:00")
 
